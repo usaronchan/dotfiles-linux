aece8404484673439b5babe924eb2c2b84e712a3
user <linux@email.com>
2025-05-19 22:22:05 +0800

    update

---
+++ scripts/vscode.sh
@@ -0,0 +1,10 @@
+
+# Download VSCode of linux-x64 via wget
+wget https://update.code.visualstudio.com/latest/linux-x64/stable -O vscode-linux-x64.tar.gz
+
+tar -xzf vscode-linux-x64.tar.gz
+
+cd VSCode-linux-x64
+mkdir data
+./code
+
