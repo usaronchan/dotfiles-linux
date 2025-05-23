f39a6f1989c95e4327e0c974a9ef005d1c10c43e
Aaron Chan <cs.yelling123@passinbox.com>
2025-05-23 14:59:13 +0800

    update

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -17,12 +17,16 @@ paccache -r
 yes | yay -Scc
 
 
+
+
 # 释放Docker内存空间
 # 清理停止的容器、未使用的网络、所有未被使用的镜像以及构建缓存，而不会弹出确认提示
 # docker system prune -a -f
 # 删除所有未被任何容器使用的卷
 # docker volume prune -f
 
-
-
+# 删除所有容器
+# podman rm -a
+# 删除所有镜像
+# podman rmi -a
 
